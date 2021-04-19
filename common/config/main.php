c229f868b5283a4b1276c6eaff1715ce918a626a
Ilya Danilov <muromizm@gmail.com>
2021-04-19 13:51:15 +0300

    RBAC

--- common/config/main.php
+++ common/config/main.php
@@ -5,9 +5,21 @@ return [
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+        ]
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager',
+        ],
+        'user' => [
+            'identityClass' => 'mdm\admin\models\User',
+            'loginUrl' => ['admin/user/login'],
+        ]
     ],
 ];
